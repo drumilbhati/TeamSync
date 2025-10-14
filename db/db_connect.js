cfad6bf1ddda970a359e1acc7a638f414cf531a5
drumilbhati <drumilbhati5@gmail.com>
2025-10-14 18:43:42 +0530

    Created folder structure for backend

---
+++ db/db_connect.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const connectDB = async () => {
+	try {
+		const connect = await mongoose.connect(process.env.MONGO_URI);
+		console.log('MongoDB connected');
+	} catch (error) {
+		console.error(error.message);
+		process.exit(1);
+	}
+}
+
+export default connectDB;
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import connectDB from './db/db_connect.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello, world!');
-});
+connectDB();
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
