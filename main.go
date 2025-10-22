d13f29e71e5fe7f4bc9a268c05ba0ea02ac02e9a
Drumil Bhati <drumilbhati5@gmail.com>
2025-10-22 08:31:37 +0530

    Added Go backend code

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/drumilbhati/teamsync/controllers"
+	"github.com/drumilbhati/teamsync/database"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	err := database.Connect(
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	defer database.Close()
+
+	r := mux.NewRouter()
+
+	// Define routes
+	r.HandleFunc("/users", controllers.GetUsers).Methods("GET")
+	r.HandleFunc("/user/{id}", controllers.GetUser).Methods("GET")
+	r.HandleFunc("/user", controllers.CreateUser).Methods("POST")
+	r.HandleFunc("/user/{id}", controllers.UpdateUser).Methods("PUT")
+	r.HandleFunc("/user/{id}", controllers.DeleteUser).Methods("DELETE")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Server starting on port: %s", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
