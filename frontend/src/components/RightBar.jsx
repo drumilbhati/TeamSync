27bd7a96a7543dcfc0d9b64aa479b6e38dca3e3a
Drumil Bhati <drumilbhati5@gmail.com>
2026-01-02 11:48:51 +0530

    add dynamic colour to task status and task priority

--- frontend/src/components/RightBar.jsx
+++ frontend/src/components/RightBar.jsx
@@ -9,6 +9,37 @@ const RightBar = () => {
   const { user } = useAuth();
   const token = user?.token;
 
+  const statusMap = {
+    todo: "TODO",
+    in_review: "IN REVIEW",
+    in_progress: "IN PROGRESS",
+    done: "DONE",
+  };
+
+  const statusColourMap = {
+    todo: "text-red-500!",
+    in_review: "text-yellow-700!",
+    in_progress: "text-blue-500!",
+    done: "text-green-500!",
+  };
+
+  const priorityOrder = {
+    high: 3,
+    medium: 2,
+    low: 1,
+  };
+
+  const priorityMap = {
+    high: "HIGH",
+    medium: "MEDIUM",
+    low: "LOW",
+  };
+  const priorityColourMap = {
+    high: "text-red-500!",
+    medium: "text-yellow-500!",
+    low: "text-green-500!",
+  };
+
   useEffect(() => {
     const fetchTasks = async () => {
       if (!selectedTeam) return;
@@ -24,7 +55,13 @@ const RightBar = () => {
 
         if (response.ok) {
           const data = await response.json();
-          setTasks(data || []);
+          const sortedData = (data || []).sort((a, b) => {
+            const priorityA = priorityOrder[a.priority] || 0;
+            const priorityB = priorityOrder[b.priority] || 0;
+            return priorityB - priorityA;
+          });
+          setTasks(sortedData);
+          console.table(sortedData);
         }
       } catch (error) {
         console.log(error);
@@ -50,13 +87,23 @@ const RightBar = () => {
       </Card>
       {tasks && tasks.length > 0 ? (
         tasks.map((task) => (
-          <Card key={task.task_id}>
+          <Card key={task.task_id} className="bg-purple-200">
             <div>
               <CardHeader>
                 <h3 className="text-teal-950!">{task.title}</h3>
               </CardHeader>
               <CardDescription>
                 <p className="text-teal-950!">{task.description?.String}</p>
+                <p className={statusColourMap[task.status] || "text-teal-950!"}>
+                  {statusMap[task.status]}
+                </p>
+                <p
+                  className={
+                    priorityColourMap[task.priority] || "text-teal-950!"
+                  }
+                >
+                  {priorityMap[task.priority]}
+                </p>
               </CardDescription>
             </div>
           </Card>
